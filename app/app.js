98f8042aaeddb32e35089cfcb66ae3b0b58055d2
MuffinPlease <benjamintec3455@gmail.com>
2023-04-04 14:14:12 -0600

    Create Server structure

--- app/app.js
+++ app/app.js
@@ -1,1 +1,30 @@
-console.log("Hello project king of diamonds ♦.");
+// const cookieParser = require("cookie-parser");
+const express = require("express");
+const path = require("path");
+const { router } = require("./routes");
+const { createServer } = require("http");
+const { tableController } = require("./controllers/tableController");
+
+const app = express();
+const httpServer = createServer(app);
+
+//Settings
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "public", "views"));
+// app.use(cookieParser());
+
+//Routes
+app.use(router);
+
+//Public
+app.use(express.static(path.join(__dirname, "public")));
+
+//Start server
+httpServer.listen(app.get("port"), () => {
+    console.log("Server running on:\t", app.get("port"));
+});
+
+//Call socket.io server
+tableController(httpServer);
+
+//Add routes
