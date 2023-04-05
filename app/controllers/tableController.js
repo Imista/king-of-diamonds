01ce5aabec5672e7a66a15f9eb3dfcafff936cbc
MuffinPlease <benjamintec3455@gmail.com>
2023-04-05 13:29:52 -0600

    Connection to tables

--- app/controllers/tableController.js
+++ app/controllers/tableController.js
@@ -4,13 +4,36 @@ function tableController(httpServer) {
     const { Server } = require("socket.io");
     const io = new Server(httpServer);
     const players = playerController();
+    const tables = [];
 
     io.on("connection", (socket) => {
-        console.log("Hello:\n", socket.id);
+        //Create table
+        socket.on("create_table", (id) => {
+            const code = Math.floor(Math.random() * Date.now())
+                .toString(16)
+                .slice(0, 5);
 
-        players.add(socket.id);
+            players.add(id);
 
-        console.log(players.data());
+            tables.push(code);
+            socket.join(code);
+
+            io.to(socket.id).emit("connected_table", code);
+        });
+
+        //Connect table
+        socket.on("connect_table", ({ id, code }) => {
+            if (tables.indexOf(code) < 0)
+                io.to(socket.id).emit(
+                    "error_table",
+                    `The table with code ${code} does not exist.`
+                );
+            else {
+                players.add(id);
+                socket.join(code);
+                io.to(code).emit("new_player", players.data());
+            }
+        });
 
         socket.on("vote", ({ id, vote }) => {
             console.log(id, " has voted.");
--- app/public/js/home.js
+++ app/public/js/home.js
@@ -1,18 +1,56 @@
 const socket = io();
-// let id = "";
+const id = Math.floor(Math.random() * Date.now()).toString(16);
+
+const stateText = document.querySelector("#state");
+const createTableButton = document.querySelector("#create-table");
+const joinTableButton = document.querySelector("#join-table");
+const tableInput = document.querySelector("#table-input");
+
+//Create
+createTableButton.addEventListener("click", () => {
+    socket.emit("create_table", id);
+});
+
+socket.on("connected_table", (table) => {
+    stateText.textContent = `connected to ${table}`;
+    console.log(table);
+    //Disable
+    tableInput.value = "";
+    tableInput.disabled = true;
+    joinTableButton.disabled = true;
+    createTableButton.disabled = true;
+});
+
+joinTableButton.addEventListener("click", () => {
+    const code = tableInput.value;
+    socket.emit("connect_table", { id, code });
+    stateText.textContent = `connected to ${code}`;
+
+    //Disable
+    tableInput.value = "";
+    tableInput.disabled = true;
+    joinTableButton.disabled = true;
+    createTableButton.disabled = true;
+});
+
+//New player
+socket.on("new_player", (data) => {
+    console.log(data);
+});
+socket.on("error_table", (msg) => {
+    alert(msg);
+});
+
+//Vote
 const voteButton = document.querySelector("#vote");
 
-socket.on("connect", () => {
-    // id = socket.id;
-    //Vote
-    voteButton.addEventListener("click", () => {
-        const voteInput = document.querySelector("#vote-input");
+voteButton.addEventListener("click", () => {
+    const voteInput = document.querySelector("#vote-input");
 
-        socket.emit("vote", {
-            id: socket.id,
-            vote: parseInt(voteInput.value),
-        });
-
-        voteInput.value = "";
+    socket.emit("vote", {
+        id,
+        vote: parseInt(voteInput.value),
     });
+
+    voteInput.value = "";
 });
